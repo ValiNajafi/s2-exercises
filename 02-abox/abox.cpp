141ca794a117d7eb38d6f930b56c7cd8ad25dd2a
V.Najafi <vali.najafi.1@gmail.com>
2022-08-22 10:19:45 +0430

    edit

--- 02-abox/abox.cpp
+++ 02-abox/abox.cpp
@@ -5,7 +5,10 @@ int main(int argc,  char const *argv[] )
     int height=atoi(argv[1]);
     std::cout << "Height = " << height << std::endl;
     int width=atoi(argv[2]);
-       std::cout << "Width = " << width << std::endl;
+    std::cout << "Width = " << width << std::endl;
+    char ch {argv[3][0]};
+    std::cout << "char = " << ch << std::endl;
+
     for (int i=1; i <= height; ++i)
     {
           for (int j=1; j <= width; ++j)
@@ -16,7 +19,7 @@ int main(int argc,  char const *argv[] )
             }
             else
             {
-                std::cout << '*' ;
+                std::cout << ch ;
             }
           }
           std::cout << std::endl;
